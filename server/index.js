78edf4b59d34c88177fb7b7246f7a06090bdc37b
santiago <santisabbioni@gmail.com>
2025-09-17 19:24:11 -0300

    cambia type commonjs a module en directorio server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
-const express = require('express');
-const dotenv = require('dotenv').config()
-const cors = require('cors')
-const mongoose = require('mongoose');
+import express from 'express'
+import mongoose from 'mongoose'
+import cookieParser from 'cookie-parser'
+import clubRoutes from './routes/clubRoutes.js'
 const app = express();
-const cookieParser = require('cookie-parser');
 
 //database connection
 mongoose.connect(process.env.MONGO_URL)
@@ -15,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/', require('./routes/clubRoutes'));
+app.use('/', clubRoutes);
 
 const port = 8000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
