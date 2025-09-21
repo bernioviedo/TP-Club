01fcabbf4d3660c573a3e034a6bc4beacc3fc8cd
santiago <santisabbioni@gmail.com>
2025-09-21 11:28:42 -0300

    merge

---
+++ client/club-frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../App.css'
+export default function Home() {
+  return (
+    <div className='main-content'>   
+      <h1>Bienvenido al Club Atlético La Gacela</h1>
+      <p>Esta es la página principal.</p>
+    </div>
+  )
+}
